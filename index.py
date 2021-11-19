bed8699535d27d87a99d30f41444a09ddc419f10
invalidcode <78639481+invalidcode232@users.noreply.github.com>
2021-11-19 18:14:26 +0700

    v2 init

--- index.py
+++ index.py
@@ -1,48 +1,63 @@
-# Auto generates a fake university profile and its faculties
-
-# Libraries
+# Import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
 import json
-import random
 
-# Remove all data from results/results.txt
-with open('results/results.txt', 'w') as f:
-    f.write('')
+# Download chromedriver using ChromeDriverManager.
+# This is supposedly better than manually specifying the path since it's already deprecated.
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-# Parse json from data/data.json
+# Delete all text in results/results.txt
+with open("results/results.txt", "w") as f:
+    f.write("")
+
+university_count = 1
+
+# Parse data/data.json
 with open('data/data.json') as json_file:
     data = json.load(json_file)
 
+    print(f"Processing {len(data['universities'])} university(s)..")
+
+    # Loop over all universities
     for university in data['universities']:
-        # Generates a random number between the 0 and length of data['connector'] and use it to get a random connector
-        connector = data['connectors']['definition'][random.randint(0, len(data['connectors']['definition']) - 1)]
+        result = f"{university_count}. {university['name']}\n\n"
 
-        # Generates a random number between the 0 and length of data['fun_facts'] and use it to get a random fun fact
-        fun_fact = data['fun_facts'][random.randint(0, len(data['fun_facts']) - 1)]
+        # Open the quipper website
+        driver.get(university['quipper'])
 
-        # Date of found
-        # Generate date of found randomly,
-        # with the minimum range specified in data['founding_date']['min'] and
-        # the maximum range specified in data['founding_date']['max']
-        founding_date = random.randint(int(data['founding_date']['min']), int(data['founding_date']['max']))
+        profile_div = driver.find_element(By.CLASS_NAME, 'school-profile__description')
+        profile = profile_div.find_element(By.TAG_NAME, 'p').get_attribute('innerHTML')
+        result += f"Profil:\n{profile}\n\n"
 
-        # Generate a random number between 0 and 1
-        style = random.randint(0, 1)
+        result += "Fakultas:\n"
+        faculties_div = driver.find_element(By.CLASS_NAME, 'school-faculties__list')
+        for faculty in faculties_div.find_elements(By.TAG_NAME, 'a'):
+            faculty_name = faculty.find_element(By.CLASS_NAME, 'faculty-item__name').get_attribute('innerHTML')
+            result += f"{faculty_name}\n"
 
-        if style == 0:
-            profile = f"Didirikan pada {founding_date}, {university} {connector} {fun_fact}."
-        else:
-            profile = f"{university} {connector} {fun_fact} yang didirikan pada {founding_date}."
+        result += "\n"
 
-        profile += "\n Fakultas: \n"
+        # The admission table we want is the first table element in the page so dw
+        try:
+            admission_table = driver.find_element(By.TAG_NAME, 'table')
+            admission_tbody = admission_table.find_element(By.TAG_NAME, 'tbody')
+            # Admission method is also the first (and only) ol in the table
+            admission_tr = admission_table.find_elements(By.TAG_NAME, 'tr')[1]
+            admission_method = admission_tr.find_element(By.TAG_NAME, 'td').get_attribute('innerText')
+            result += f"Jalur masuk:\n{admission_method}\n\n"
+            # print(admission_method)
+        except NoSuchElementException:
+            print(f"No admission table for university {university['name']}")
 
-        # Generate a random number and use it as the faculty count, then get the faculties based on the count randomly,
-        # the faculties must be unique
-        faculty_count = random.randint(1, len(data['faculties']))
-        faculties = random.sample(data['faculties'], faculty_count)
+        print(result)
 
-        for faculty in faculties:
-            profile += f"\n - {faculty}"
+        # Write the results to results/results.txt
+        with open("results/results.txt", "a") as f:
+            f.write(result)
 
-        # Append profile to results/results.txt
-        with open('results/results.txt', 'a') as results:
-            results.write(profile + "\n\n")
+        print(f"Finished processing {university['name']} ({university_count})")
+
+        university_count += 1
